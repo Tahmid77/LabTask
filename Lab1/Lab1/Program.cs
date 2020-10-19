d0deb8231c4bf5908e63681de6ce3cbb3033cbb7
Tahmid77 <19-41077-2@student.aiub.edu>
2020-10-19 11:02:26 +0600

    Added

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task no. 1
+
+            String name = "Tahmid";
+            System.Console.WriteLine("Hello");
+            System.Console.WriteLine(name);
+
+
+            //Task no. 2
+            /*
+            int a = 2;
+            int b = 3;
+            int sum = a+b ;
+            System.Console.WriteLine("Sum = " + sum);
+            */
+
+            //Task no. 3
+            /*
+            int a = 5;
+            float b = 2.5f;
+            float div = a / b;
+            System.Console.WriteLine("Result = " + div);
+            */
+
+            //Task no. 4
+            /*
+            double a = -5 + 8 * 6;
+            double b = (55 + 9) % 9;
+            double c = 20 + -3 * 5 / 8;
+            double d = 5 + 15 / 3 * 2 - 8 % 3;
+            System.Console.WriteLine("-5 + 8 * 6 = " + a);
+            System.Console.WriteLine("(55 + 9) % 9 = " + b);
+            System.Console.WriteLine("20 + -3*5 / 8 = " + c);
+            System.Console.WriteLine("5 + 15 / 3 * 2 - 8 % 3 = " + d);
+            */
+
+            //Task no. 5
+            /*
+            int a = 5;
+            double b = 2;
+            double sum, mul, sub, div, rem;
+            sum = a + b;
+            mul = a / b;
+            sub = a - b;
+            div = a / b;
+            rem = a % b;
+            System.Console.WriteLine("5+2 = "+sum+"\n"+"5-2 = "+sub+"\n"+"5*2 = "+mul+"\n"+"5/2 = "+div+"\n"+"5%2 = "+rem);
+            */
+
+            //Task no. 6
+            /*
+            double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            System.Console.WriteLine("((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = "+result);
+            */
+
+            //Task no. 7
+            /*
+            double w, h, a, p;
+            w = 5.6;
+            h = 8.5;
+            a = w * h;
+            p = 2 * (w + h);
+            System.Console.WriteLine("Area is 5.6 * 8.5 = "+a+"\n"+ "Perimeter is 2 * (5.6 + 8.5) = "+p);
+            */
+        }
+    }
+}
